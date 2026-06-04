2c3803aec042c634833b56dfd675f79c322cb982
san-anselmo <edzelfrincebual@gmail.com>
2026-06-04 12:20:48 +0800

    Update components.js

--- js/components.js
+++ js/components.js
@@ -374,7 +374,6 @@ function injectFooter() {
         </div>
         <div class="footer-meta-info">
           <span class="footer-meta-item">v1.0.10</span>
-          <span class="footer-meta-item footer-status-online">● CMS Connected</span>
         </div>
         <div class="footer-credit">
           <a href="https://www.facebook.com/edzfrnc" target="_blank" rel="noopener">Developed and designed by Edzel Frince Bual.</a>
